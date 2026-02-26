4c9650aa0245d865989d8fbbf9408c6f1b0c5b83
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2026-02-26 16:36:41 +0900

    Fix : vercel 배포 주소 추가

--- backend/src/main/java/com/example/momentum_backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/momentum_backend/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // Vite
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://momentum-sigma-rosy.vercel.app")); // Vite
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
