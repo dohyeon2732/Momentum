bbdd340f145ef5bcbd7aa1868c779467973a45ad
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2026-02-27 15:52:46 +0900

    Fix : login error fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react"
 import AppRouter from "./router/Router"
+import api from "./apis/axios";
 
 function App() {
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if(token){
+      api.get("/users/me")
+      .catch(()=>{
+        localStorage.removeItem("accessToken");
+        window.location.href = "/login";
+      })
+    }
+  },[]);
 
   return (
     <div className="flex flex-col items-center justify-center">
